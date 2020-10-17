a014b5893797049068a8f541a7aca4df309dfd78
Isaayy <kuba.serwin99@gmail.com>
2020-10-17 11:48:33 +0200

    New nav

--- script.js
+++ script.js
@@ -2,8 +2,11 @@ let nav = document.getElementById('header__menu');
 
 let bar = document.getElementById('hamburger-bar');
 
+let htmlElement = document.querySelector("html");
+
 nav.addEventListener('click',function(){
     nav.classList.toggle('header--active');
+    htmlElement.classList.toggle('hidden');
 });
 
 
@@ -11,7 +14,9 @@ let list = document.getElementsByClassName('nav-link');
 
 for (let i = 0 ; i < list.length ; i++){
     list[i].addEventListener('click',function(){
+        
         nav.classList.toggle('header--active');
+        htmlElement.classList.toggle('hidden');
     })
 }
 
