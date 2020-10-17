7ec10fdf39c78fde1e1b4d03a74a606bda702f5c
Isaayy <kuba.serwin99@gmail.com>
2020-10-17 12:42:22 +0200

    Different nav

--- script.js
+++ script.js
@@ -2,9 +2,13 @@ let nav_box  = document.getElementById('header__nav-box');
 let nav = document.getElementById('header__nav');
 let nav_outside = document.getElementById('header__nav-outside');
 
+let htmlElement = document.querySelector('html');
+
+
 nav_outside.addEventListener('click',function(){
     nav_box.classList.toggle('header--active');
     nav.classList.toggle('header--transparent');
+    htmlElement.classList.toggle('u-hidden');
 });
 
 
@@ -14,5 +18,6 @@ for ( let i = 0 ; i < links.length ; i++){
 
     links[i].addEventListener('click',function(){
         nav_box.classList.toggle('header--active');
+        htmlElement.classList.toggle('u-hidden');
     });
 };
