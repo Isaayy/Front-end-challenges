8f223cdf05f8bda6d3e7330c693e99b6e9679554
Isaayy <kuba.serwin99@gmail.com>
2020-11-03 17:02:15 +0100

    Hamburger menu layout and effect

--- script.js
+++ script.js
@@ -1,2 +1,17 @@
 'use strict';
 
+const hamburger = document.querySelector('.hamburger__outline');
+const bar = document.querySelector('.hamburger__bar');
+
+hamburger.addEventListener('click',function(){
+    hamburger.classList.toggle('hamburger--active');
+    
+    if (bar.classList.contains('hamburger__bar--hidden')){
+        setTimeout(function(){
+            bar.classList.remove('hamburger__bar--hidden'); }, 100);
+    }
+    else {
+        bar.classList.add('hamburger__bar--hidden');
+    }
+});
+
