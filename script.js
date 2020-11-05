57518f1ffc9526ba64de8ce52c1f7ddb69607047
Isaayy <kuba.serwin99@gmail.com>
2020-11-05 10:28:54 +0100

    Features section layout + links hover animation

--- script.js
+++ script.js
@@ -2,16 +2,18 @@
 
 const hamburger = document.querySelector('.hamburger__outline');
 const bar = document.querySelector('.hamburger__bar');
+const nav = document.querySelector('.header__menu');
 
 hamburger.addEventListener('click',function(){
     hamburger.classList.toggle('hamburger--active');
+    nav.classList.toggle('header__menu--moveIn');
     
-    if (bar.classList.contains('hamburger__bar--hidden')){
+    if (bar.classList.contains('hamburger__bar--hidden')){ // on click where X ( bards crossed)
         setTimeout(function(){
             bar.classList.remove('hamburger__bar--hidden'); }, 100);
     }
     else {
-        bar.classList.add('hamburger__bar--hidden');
+        bar.classList.add('hamburger__bar--hidden'); // on first click
     }
 });
 
