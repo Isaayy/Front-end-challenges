c20fe0afe7501acd0721ca34f9d7d34da77fc23c
Isaayy <kuba.serwin99@gmail.com>
2020-11-09 08:42:26 +0100

    Features media queries

--- script.js
+++ script.js
@@ -17,3 +17,30 @@ hamburger.addEventListener('click',function(){
     }
 });
 
+const text = document.querySelectorAll(".features__description");
+
+// const mediaQueries = {
+//     tablet : '(max-width: 768px)',
+//     mobile : '(max-width: 576px)'
+// }
+
+const applyMedia = x => {
+    if(x.matches){
+        for(let item of text)
+            item.textContent = 'Lorem ipsum dolor sit amet, consectetur adipiscing elit. Velit, massa orci viverra aliquet egestas cras. Lectus urna nulla dui morbi tortor, egestas a aliquam.';
+    }
+    else {
+        for(let item of text)
+        item.textContent = 'Lorem ipsum dolor sit amet, consectetur adipiscing elit. Velit, massa orci viverra aliquet egestas cras. Lectus urna nulla dui morbi tortor, egestas a aliquam. Sed lectus sodales eget vitae potenti non adipiscing malesuada nulla. Integer quam habitant tellus sed eget mauris in libero.';
+    }
+}
+
+let media = window.matchMedia('(max-width: 768px)');
+applyMedia(media ) // Call listener function at run time
+media .addListener(applyMedia) // Attach listener function on state changes
+
+
+
+
+
+
