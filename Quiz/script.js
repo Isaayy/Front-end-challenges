bf4156d01070685ca4ea7164e90612efc4a1f5d6
Isaayy <kuba.serwin99@gmail.com>
2020-11-27 16:07:11 +0100

    Layout v2

--- Quiz/script.js
+++ Quiz/script.js
@@ -1,1 +1,21 @@
 'use strict';
+
+const htmlBtn = document.querySelector('.quiz-type__html');
+const cssBtn = document.querySelector('.quiz-type__css');
+const jsBtn = document.querySelector('.quiz-type__js');
+
+const menu = document.querySelector('.menu');
+const questionsBox = document.querySelector('.questions-box');
+
+htmlBtn.addEventListener('click', () => {
+  hideMenu();
+  showQuestionsBox();
+});
+
+const hideMenu = () => {
+  menu.classList.add('swipeLeft');
+};
+
+const showQuestionsBox = () => {
+  questionsBox.classList.add('swipeRight');
+};
