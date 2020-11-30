75056cc975d15a50b03d161da89ca850a960c56a
Isaayy <kuba.serwin99@gmail.com>
2020-11-30 14:24:44 +0100

    html quiz

--- Quiz/script.js
+++ Quiz/script.js
@@ -1,15 +1,99 @@
 'use strict';
 
+// ########################################################
+// ########################################################
+// ########################################################
+// MAIN FUNCTIONALITY
+
+const questionNumber = document.querySelector('.question-number');
+
+let quizType;
+let currentQuestionNuber = 1;
+let goodAnswers = 0;
+
+const displayCurrentQuestionNumber = () => {
+  questionNumber.textContent = `${currentQuestionNuber}/5`;
+};
+
+displayCurrentQuestionNumber();
+
+// ########################################################
+// ########################################################
+// ########################################################
+// START QUIZ - DISPLAYING QUESTIONS AND ANSWERS
+
+const htmlBtn = document.querySelector('.quiz-type__html');
+const cssBtn = document.querySelector('.quiz-type__css');
+const jsBtn = document.querySelector('.quiz-type__js');
+
+const menu = document.querySelector('.menu');
+const questionsBox = document.querySelector('.questions-box');
+const resultsBox = document.querySelector('.result-box');
+
+htmlBtn.addEventListener('click', () => {
+  startQuiz('html');
+});
+
+cssBtn.addEventListener('click', () => {
+  startQuiz('css');
+});
+
+jsBtn.addEventListener('click', () => {
+  startQuiz('js');
+});
+
+const startQuiz = (type) => {
+  quizType = type;
+  setAnswersColor();
+  menu.classList.add('swipeLeft');
+  questionsBox.classList.add('swipeFromRight');
+  draftQuestion(); // ! draft different questions for different quizes
+};
+
+// different background color for different technology
+const setAnswersColor = () => {
+  let answerBgc;
+  if (quizType === 'html') {
+    answerBgc = 'pink-red';
+  } else if (quizType === 'css') {
+    answerBgc = 'teal';
+  } else {
+    answerBgc = 'orange';
+  }
+  for (let i = 0; i < answers.children.length; i++) {
+    answers.children[i].classList.add(`${answerBgc}`);
+  }
+};
+
+// ########################################################
+// ########################################################
+// ########################################################
+// END QUIZ - HIDE QUESTIONS AND ANSWERS, SHOW RESULT
+
+const hideQuestionsBox = () => {
+  questionsBox.classList.toggle('display-none');
+};
+
+const showResultsBox = () => {
+  resultsBox.classList.toggle('display-none');
+};
+
+const endGame = () => {
+  hideQuestionsBox();
+  showResultsBox();
+};
+
 // ############################
 // DRAFTING QUESTIONS
+// TODO DIFFERENT QUESIONS DRAFTING FOR DIFFEREN QUIZES
 
 const answers = document.querySelector('.questions-box__answers');
 const question = document.querySelector('.question');
 
 let htmlQuestions = {
-  question: [
+  questions: [
     'HTML stands for?',
-    'How is document type initialized in HTML5.?',
+    'How is document type initialized in HTML5?',
     'Which of the following HTML Elements is used for making any text bold ?',
     'Which of the following HTML element is used for creating an unordered list?',
     'How many heading tags are there in HTML5?',
@@ -44,7 +128,15 @@ let htmlQuestions = {
     '<body>',
   ],
   d: ['	None of these', '</DOCTYPE html>', '<b>', '<ul>', '5', 'src', '<form>'],
-  goodAnswer: ['a', 'c', 'd', 'd', 'c', 'b', 'a'],
+  goodAnswer: [
+    'Hyper Text Markup Language',
+    '<!DOCTYPE HTML>',
+    '<b>',
+    '<ul>',
+    '6',
+    'href',
+    '<html>',
+  ],
 };
 
 function getRandomInt(max) {
@@ -54,74 +146,45 @@ function getRandomInt(max) {
 let randomNumber;
 let answeredQuestions = [];
 
-const draftQuestions = (quiz) => {
-  if (quiz === 'html') {
+const draftQuestion = () => {
+  if (quizType === 'html') {
     // random number
     randomNumber = getRandomInt(7);
     if (!answeredQuestions.includes(randomNumber)) {
-      question.textContent = htmlQuestions.question[randomNumber];
+      question.textContent = htmlQuestions.questions[randomNumber];
       answers.children[0].textContent = htmlQuestions.a[randomNumber];
       answers.children[1].textContent = htmlQuestions.b[randomNumber];
       answers.children[2].textContent = htmlQuestions.c[randomNumber];
       answers.children[3].textContent = htmlQuestions.d[randomNumber];
       answeredQuestions.push(randomNumber);
     } else {
-      draftQuestions('html');
+      draftQuestion('html');
     }
   }
 };
 
 // ############################
-// DISPLAYING QUESTIONS AND ANSWERS
+// CHECK IN ANSWER IS GOOD OR NOT
 
-const htmlBtn = document.querySelector('.quiz-type__html');
-const cssBtn = document.querySelector('.quiz-type__css');
-const jsBtn = document.querySelector('.quiz-type__js');
-
-const menu = document.querySelector('.menu');
-const questionsBox = document.querySelector('.questions-box');
-
-htmlBtn.addEventListener('click', () => {
-  displayQuestions('html');
-  hideMenu();
-  showQuestionsBox();
-  draftQuestions('html');
-});
-
-cssBtn.addEventListener('click', () => {
-  displayQuestions('css');
-  hideMenu();
-  showQuestionsBox();
-});
-
-jsBtn.addEventListener('click', () => {
-  displayQuestions();
-  hideMenu();
-  showQuestionsBox();
-});
-
-const hideMenu = () => {
-  menu.classList.add('swipeLeft');
+const isGoodAnswer = (answer) => {
+  if (answer.textContent === htmlQuestions.goodAnswer[randomNumber])
+    return true;
+  return false;
 };
 
-const showQuestionsBox = () => {
-  questionsBox.classList.add('swipeRight');
-};
-
-const displayQuestions = (quizType) => {
-  let answerBgc; // different background color for different technology
-  if (quizType === 'html') {
-    answerBgc = 'pink-red';
-  } else if (quizType === 'css') {
-    answerBgc = 'teal';
-  } else {
-    answerBgc = 'orange';
-  }
-  for (let i = 0; i < answers.children.length; i++) {
-    answers.children[i].classList.add(`${answerBgc}`);
-  }
-};
+for (let i = 0; i < answers.children.length; i++) {
+  answers.children[i].addEventListener('click', () => {
+    if (isGoodAnswer(answers.children[i])) {
+      goodAnswers++;
+    }
+    if (currentQuestionNuber === 5) endGame();
+    else {
+      currentQuestionNuber++;
+      displayCurrentQuestionNumber();
+      draftQuestion();
+    }
+  });
+}
 
 // TODO
 // DIFFERENT COLOR FOR JS - CHECK CONTRAST
-// on answer Click add event listener to check if the answer is correct , call draft question <- reapeat until questionNumber <= 5
