015a465552d8b2db7c73dc43eae21c1f95dc11ed
Isaayy <kuba.serwin99@gmail.com>
2020-12-08 20:23:08 +0100

    Components in password

--- password-generator/script.js
+++ password-generator/script.js
@@ -1,1 +1,137 @@
 'use strict';
+
+const generateBtn = document.querySelector('.generate-btn');
+const copyBtn = document.querySelector('.copyBtn');
+
+const length = document.querySelector('.pass-length');
+
+// password components
+
+const uppercase = [
+  'A',
+  'B',
+  'C',
+  'D',
+  'E',
+  'F',
+  'G',
+  'H',
+  'I',
+  'J',
+  'K',
+  'L',
+  'M',
+  'N',
+  'O',
+  'P',
+  'Q',
+  'R',
+  'S',
+  'T',
+  'U',
+  'V',
+  'W',
+  'X',
+  'Y',
+  'Z',
+];
+
+const lowercase = [
+  'a',
+  'b',
+  'c',
+  'd',
+  'e',
+  'f',
+  'g',
+  'h',
+  'i',
+  'j',
+  'k',
+  'l',
+  'm',
+  'n',
+  'o',
+  'p',
+  'q',
+  'r',
+  's',
+  't',
+  'u',
+  'v',
+  'w',
+  'x',
+  'y',
+  'z',
+];
+
+const numbers = ['1', '2', '3', '4', '5', '6', '7', '8', '9'];
+
+const symbols = [
+  '!',
+  '@',
+  '#',
+  '$',
+  '%',
+  '^',
+  '&',
+  '*',
+  '(',
+  ')',
+  '[',
+  ']',
+  ',',
+  '.',
+  '/',
+  '?',
+  '+',
+  '=',
+  '-',
+  '_',
+];
+
+let passwordComponents;
+
+// Generate password
+
+let password;
+let passwordLength = 0;
+let components;
+
+const whichComponents = () => {
+  passwordComponents = [];
+
+  const includeLowercase = document.querySelector('.include-lowercase');
+  const includeUppercase = document.querySelector('.include-uppercase');
+  const includeNumbers = document.querySelector('.include-numbers');
+  const includeSymbols = document.querySelector('.include-symbols');
+
+  if (includeLowercase.checked) passwordComponents.push(lowercase);
+  if (includeUppercase.checked) passwordComponents.push(uppercase);
+  if (includeNumbers.checked) passwordComponents.push(numbers);
+  if (includeSymbols.checked) passwordComponents.push(symbols);
+};
+
+const generatePassword = () => {
+  passwordLength = length.value;
+  password = '';
+  whichComponents();
+  while (passwordLength) {
+    let randomIndex = randomNumber(passwordComponents.length);
+    let componentLength = passwordComponents[randomIndex].length;
+    password += passwordComponents[randomIndex][randomNumber(componentLength)];
+    passwordLength--;
+  }
+
+  console.log(passwordComponents.length);
+  console.log(password);
+};
+
+const randomNumber = (max) => {
+  return Math.floor(Math.random() * Math.floor(max));
+};
+
+generateBtn.addEventListener('click', generatePassword);
+
+// TODO ADD IF EMPTY SIZE
+// copy to clipboard
