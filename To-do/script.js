643f0ae82480b445dfec9e3f1d908e19139e9b7e
Isaayy <kuba.serwin99@gmail.com>
2021-01-07 00:01:32 +0100

    Time and date

--- To-do/script.js
+++ To-do/script.js
@@ -1,1 +1,33 @@
 'use strict';
+
+// DATE & TIME
+
+const timeBox = document.querySelector('.time-box');
+const dateBox = document.querySelector('.date-box');
+
+let now, hours, minutes;
+const clock = () => {
+  now = new Date();
+  hours = now.getHours();
+  minutes = now.getMinutes();
+
+  hours < 10 ? (hours = `0${hours}`) : hours;
+  minutes < 10 ? (minutes = `0${minutes}`) : minutes;
+
+  timeBox.textContent = `${hours}:${minutes}`;
+};
+setInterval(clock, 1000);
+
+const setDate = () => {
+  now = new Date();
+  let weekDays = ['Sunday', 'Monday', 'Tuesday', 'Wednesday', 'Thursday', 'Friday', 'Saturday'];
+  let months = ['January', 'February', 'March', 'April', 'May', 'Juny', 'July', 'August', 'September', 'October', 'November', 'December'];
+
+  let weekDay = weekDays[now.getDay()];
+  let day = now.getDate();
+  let month = months[now.getMonth()];
+
+  dateBox.textContent = `${weekDay}, ${day} ${month}`;
+};
+
+setDate();
