d620ddc32885b1300ced1fbd761643525e98b0b7
Isaayy <kuba.serwin99@gmail.com>
2021-02-09 12:01:34 +0100

    Answers functionality

--- faq-accordion-card-main/script.js
+++ faq-accordion-card-main/script.js
@@ -1,1 +1,17 @@
 'use strict';
+
+const buttons = document.querySelectorAll('.img-btn');
+
+for (const button of buttons) {
+  button.addEventListener('click', function () {
+    let panel = this.parentElement.nextElementSibling;
+    if (panel.style.maxHeight) {
+      panel.style.maxHeight = null;
+    } else {
+      panel.style.maxHeight = panel.scrollHeight + 'px';
+    }
+    panel.classList.toggle('margin-top-show');
+    this.classList.toggle('arrow-flip');
+    this.parentElement.classList.toggle('bold-font');
+  });
+}
