515726d0b0ce9fc9b2d43273ce82d4668135c8c8
Isaayy <kuba.serwin99@gmail.com>
2021-02-17 12:33:36 +0100

    Form validation

---
+++ huddle-landing-page-with-curved-sections-master/script.js
@@ -0,0 +1,21 @@
+'use strict';
+
+const isValid = value => {
+  const re = /^(?:[a-z0-9!#$%&'*+/=?^_`{|}~-]+(?:\.[a-z0-9!#$%&'*+/=?^_`{|}~-]+)*|"(?:[\x01-\x08\x0b\x0c\x0e-\x1f\x21\x23-\x5b\x5d-\x7f]|\\[\x01-\x09\x0b\x0c\x0e-\x7f])*")@(?:(?:[a-z0-9](?:[a-z0-9-]*[a-z0-9])?\.)+[a-z0-9](?:[a-z0-9-]*[a-z0-9])?|\[(?:(?:(2(5[0-5]|[0-4][0-9])|1[0-9][0-9]|[1-9]?[0-9]))\.){3}(?:(2(5[0-5]|[0-4][0-9])|1[0-9][0-9]|[1-9]?[0-9])|[a-z0-9-]*[a-z0-9]:(?:[\x01-\x08\x0b\x0c\x0e-\x1f\x21-\x5a\x53-\x7f]|\\[\x01-\x09\x0b\x0c\x0e-\x7f])+)\])$/;
+  return re.test(String(value).toLowerCase());
+};
+
+const btn = document.querySelector('.form-btn');
+const validMessage = document.querySelector('.form-valid-message');
+const inputField = document.querySelector('.form-input');
+
+btn.addEventListener('click', e => {
+  e.preventDefault();
+  if (!isValid(inputField.value)) {
+    validMessage.classList.remove('hidden');
+    inputField.classList.add('form-input--red');
+  } else {
+    inputField.classList.remove('form-input--red');
+    validMessage.classList.add('hidden');
+  }
+});
