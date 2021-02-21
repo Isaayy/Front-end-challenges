0d5102052e3183ced1073725328c1b2bf33ba162
Isaayy <kuba.serwin99@gmail.com>
2021-02-21 15:12:34 +0100

    generateArray and countWords improvement

--- word-frequency/script.js
+++ word-frequency/script.js
@@ -5,57 +5,80 @@ const input = document.querySelector('.input');
 
 countBtn.addEventListener('click', () => {
   if (input.value) {
-    reset();
-    generateArray(input.value);
+    table.innerHTML = '';
+    generateResult(input.value);
   }
 });
 
 // ############################################################
-let results = [];
+// GENERATE ARRAY OF FORMATTED WORDS
 
 const generateArray = str => {
-  // remove unnecessary spaces
-  str = str.replaceAll('  ', '').trim();
+  // remove unnecessary whitespace
+  str = str.replace(/\s+/g, ' ').trim();
 
-  // create array of words
-  const words = str.split(' ');
+  // remove symbols
+  str = str.replace(/[^a-zA-Z ]/g, '');
 
-  // Word frequency in array of words
-  let count = 1;
-  let found;
-
-  for (let i = 0; i < words.length; i++) {
-    for (let k = i + 1; k < words.length; k++) {
-      if (words[i] === words[k]) {
-        count++;
-      }
-    }
-    // check if the word is not already in results
-    found = results.some(el => el.word === words[i]);
-    if (!found) results.push({ word: words[i], frequency: count });
-
-    count = 1;
-  }
-  // sort words by frequency
-  results.sort((a, b) => (a.frequency < b.frequency ? 1 : -1));
-
-  generateResult();
+  // return array of lowercase words
+  return str.split(' ').map(el => el.toLowerCase());
 };
 
 // ############################################################
+// COUNT WORD FREQUENCY
 
+const countWords = str => {
+  // Get array of words
+  const words = generateArray(str);
+
+  // Count words
+  let result = {};
+  for (const word of words) {
+    result[word] = result[word] + 1 || 1;
+  }
+
+  // sort words by frequency
+  result = Object.entries(result).sort((a, b) => b[1] - a[1]);
+
+  // return sorted object
+  return Object.fromEntries(result);
+};
+
+// ############################################################
+// Generate result table
 const table = document.querySelector('.results-table');
 const resultsBox = document.querySelector('.results');
 
-const generateResult = () => {
+const generateResult = str => {
+  // Show table
   resultsBox.classList.remove('hidden');
-  for (let i = 0; i < results.length; i++) {
-    table.innerHTML += `<tr><td>${i + 1}</td><td>${results[i].word}</td><td>${results[i].frequency}</td></tr>`;
+
+  // Get sorted object
+  const results = countWords(str);
+
+  // Insert data from object into table
+
+  // forEach
+  Object.entries(results).forEach((entry, i) => (table.innerHTML += `<tr><td>${i + 1}</td><td>${entry[0]}</td><td>${entry[1]}</td></tr>`));
+
+  // for in
+  let i = 1;
+  for (const word in results) {
+    table.innerHTML += `<tr><td>${i}</td><td>${word}</td><td>${results[word]}</td></tr>`;
+    i++;
+  }
+
+  // for of
+  let i = 1;
+  for (const entry of Object.entries(results)) {
+    const [value, key] = entry;
+    table.innerHTML += `<tr><td>${i}</td><td>${value}</td><td>${key}</td></tr>`;
+    i++;
   }
 };
 
 // ############################################################
-
+// Display more words
 const showMoreBtn = document.querySelector('.show-more');
 
 showMoreBtn.addEventListener('click', () => {
@@ -63,10 +86,3 @@ showMoreBtn.addEventListener('click', () => {
   if (showMoreBtn.textContent === `Show less`) showMoreBtn.innerHTML = `Show more`;
   else showMoreBtn.innerHTML = `Show less`;
 });
-
-// ############################################################
-
-const reset = () => {
-  results = [];
-  table.innerHTML = '';
-};
