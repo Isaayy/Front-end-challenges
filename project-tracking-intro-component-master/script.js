8f2d55e4f0f809908d67a0ffb90e5c0c535e0c23
Isaayy <kuba.serwin99@gmail.com>
2021-02-24 12:16:30 +0100

    Hamburger menu

--- project-tracking-intro-component-master/script.js
+++ project-tracking-intro-component-master/script.js
@@ -1,1 +1,13 @@
 'use strict';
+
+const hamburger = document.querySelector('.hamburger__box');
+const nav = document.querySelector('.header__list');
+const links = document.querySelectorAll('.header__link');
+
+const toggleNav = () => {
+  hamburger.firstElementChild.classList.toggle('hamburger__bar--active');
+  nav.classList.toggle('header__list--active');
+};
+
+hamburger.addEventListener('click', toggleNav);
+links.forEach(link => link.addEventListener('click', toggleNav));
