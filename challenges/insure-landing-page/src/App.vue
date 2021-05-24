8f5d7235e48ec00755d36042411946ef6ed03205
Isaayy <kuba.serwin99@gmail.com>
2021-05-24 08:52:44 +0200

    init

---
+++ challenges/insure-landing-page/src/App.vue
@@ -0,0 +1,26 @@
+<template>
+  <h1>App</h1>
+</template>
+
+<script>
+export default {
+  name: 'App',
+};
+</script>
+
+<style lang="scss">
+@import './assets/styles/variables.css';
+
+*,
+*::after,
+*::before {
+  margin: 0;
+  padding: 0;
+  box-sizing: border-box;
+}
+
+html {
+  font-size: 62.5%;
+  scroll-behavior: smooth;
+}
+</style>
