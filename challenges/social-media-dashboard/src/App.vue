2d78daf98ad6270605ef092214c3201a52af0132
Isaayy <kuba.serwin99@gmail.com>
2021-05-27 10:47:14 +0200

    init

---
+++ challenges/social-media-dashboard/src/App.vue
@@ -0,0 +1,29 @@
+<template>
+  <h1>app</h1>
+</template>
+
+<script>
+export default {
+  name: 'App',
+  components: {},
+};
+</script>
+
+<style lang="scss">
+@import './assets/abstracts/variables.css';
+
+*,
+*::after,
+*::before {
+  margin: 0;
+  padding: 0;
+  box-sizing: border-box;
+}
+
+html {
+  font-size: 62.5%;
+  scroll-behavior: smooth;
+  font-family: 'Inter', sans-serif;
+  font-size: 1.4rem;
+}
+</style>
